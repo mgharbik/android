b2ec75ecb78c8ae380f4ba5cb65d3f6211f6e644
Gharbi Mohammed <simo163@hotmail.co.uk>
2014-03-02 15:21:25 +0100

    Set the android app, Splash and StartingPoint pages

---
+++ src/com/thenewboston/travis/Splash.java
@@ -0,0 +1,36 @@
+package com.thenewboston.travis;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash extends Activity{
+
+	@Override
+	protected void onCreate(Bundle TravisLoveBacon) {
+		// TODO Auto-generated method stub
+		super.onCreate(TravisLoveBacon);
+		setContentView(R.layout.splash);
+		Thread timer = new Thread(){
+			public void run(){
+				try{
+					sleep(5000);
+				} catch (InterruptedException e){
+					e.printStackTrace();
+				} finally{
+					Intent openStartingPoint = new Intent("com.thenewboston.travis.STARTINGPOINT");
+					startActivity(openStartingPoint);
+				}
+			}
+		};
+		timer.start();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
+	
+}
