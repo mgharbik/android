b904a0c5ddba1c7b3ac9401ff786c74623ca9aed
Gharbi Mohammed <simo163@hotmail.co.uk>
2014-03-02 15:32:01 +0100

    add music to the first layout showed up

--- src/com/thenewboston/travis/Splash.java
+++ src/com/thenewboston/travis/Splash.java
@@ -2,15 +2,20 @@ package com.thenewboston.travis;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class Splash extends Activity{
-
+	
+	MediaPlayer ourSong;
+	
 	@Override
 	protected void onCreate(Bundle TravisLoveBacon) {
 		// TODO Auto-generated method stub
 		super.onCreate(TravisLoveBacon);
 		setContentView(R.layout.splash);
+		ourSong = MediaPlayer.create(Splash.this, R.raw.splashsound);
+		ourSong.start();
 		Thread timer = new Thread(){
 			public void run(){
 				try{
@@ -30,6 +35,7 @@ public class Splash extends Activity{
 	protected void onPause() {
 		// TODO Auto-generated method stub
 		super.onPause();
+		ourSong.release();
 		finish();
 	}
 	
