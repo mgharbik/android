63f8e22ad99799f30ef9424cb8ab5ea1c855df10
Gharbi Mohammed <simo163@hotmail.co.uk>
2014-04-10 20:37:05 +0200

    Passing string between activities, StartActivityForResult(), setResult()

--- src/com/thenewboston/travis/OpenedClass.java
+++ src/com/thenewboston/travis/OpenedClass.java
@@ -1,13 +1,13 @@
 package com.thenewboston.travis;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.TextView;
 
 public class OpenedClass extends Activity implements OnClickListener, OnCheckedChangeListener{
@@ -15,6 +15,7 @@ public class OpenedClass extends Activity implements OnClickListener, OnCheckedC
 	Button returnData;
 	TextView question, test;
 	RadioGroup selectionList;
+	String gotBread, setData;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +23,9 @@ public class OpenedClass extends Activity implements OnClickListener, OnCheckedC
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.send);
 		initialize();
+		//Bundle gotBasket = getIntent().getExtras();
+		//gotBread = gotBasket.getString("key");
+		//question.setText(gotBread);
 	}
 
 	private void initialize() {
@@ -30,26 +34,34 @@ public class OpenedClass extends Activity implements OnClickListener, OnCheckedC
 		question = (TextView) findViewById(R.id.tvQuestion);
 		test = (TextView) findViewById(R.id.tvText);
 		returnData.setOnClickListener(this);
+		selectionList = (RadioGroup) findViewById(R.id.rgAnswers);
 		selectionList.setOnCheckedChangeListener(this);
 	}
 
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		
+		Intent person = new Intent();
+		Bundle backpack = new Bundle();
+		backpack.putString("answer", setData);
+		person.putExtras(backpack);
+		setResult(RESULT_OK, person);
+		finish();
 	}
 
 	public void onCheckedChanged(RadioGroup arg0, int arg1) {
 		// TODO Auto-generated method stub
 		switch(arg1){
 		case R.id.rMessi:
-			
+			setData = "Correct";
 			break;
 		case R.id.rRonaldo:
-			
+			setData = "Not Correct";
 			break;
 		case R.id.rNeymar:
-			
+			setData = "Not Correct";
 			break;
 		}
+		test.setText(setData);
 	}
+
 }
