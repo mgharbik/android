0a6159025187ed40fb22dd205cd8c507d92d3f05
Gharbi Mohammed <simo163@hotmail.co.uk>
2014-04-13 15:51:21 +0200

    Typeface and the asset folder

--- src/com/thenewboston/travis/MyBringBack.java
+++ src/com/thenewboston/travis/MyBringBack.java
@@ -6,19 +6,23 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Paint.Align;
 import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.view.View;
 
 public class MyBringBack extends View {
 
 	Bitmap gBall;
 	float changingY;
+	Typeface font;
 	
 	public MyBringBack(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
 		gBall = BitmapFactory.decodeResource(getResources(), R.drawable.green_ball);
 		changingY = 0;
+		font = Typeface.createFromAsset(context.getAssets(), "G-Unit.TTF");
 	}
 
 	@Override
@@ -26,12 +30,21 @@ public class MyBringBack extends View {
 		// TODO Auto-generated method stub
 		super.onDraw(canvas);
 		canvas.drawColor(Color.WHITE);
+		
+		Paint textPaint = new Paint();
+		textPaint.setARGB(50, 254, 10, 50);
+		textPaint.setTextAlign(Align.CENTER);
+		textPaint.setTextSize(50);
+		textPaint.setTypeface(font);
+		canvas.drawText("mybringback", (canvas.getWidth())/2, 200, textPaint);
+		
 		canvas.drawBitmap(gBall, (canvas.getWidth()/2), changingY, null);
 		if (changingY < canvas.getHeight()){
 			changingY += 10;
 		}else{
 			changingY = 0;
 		}
+		
 		Rect middleRect = new Rect();
 		middleRect.set(0, 400, canvas.getWidth(), 550);
 		Paint ourBlue = new Paint();
