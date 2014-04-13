03cde5b7a2a28a6a377fe5246a3db838fe68dd31
Gharbi Mohammed <simo163@hotmail.co.uk>
2014-04-13 14:38:36 +0200

    Creating a custom button

--- gen/com/thenewboston/travis/R.java
+++ gen/com/thenewboston/travis/R.java
@@ -25,9 +25,13 @@ public final class R {
         public static final int activity_vertical_margin=0x7f070001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int splash_background=0x7f020001;
-        public static final int splash_background1=0x7f020002;
+        public static final int custom=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int plus=0x7f020002;
+        public static final int plus_highlighted=0x7f020003;
+        public static final int plus_selected=0x7f020004;
+        public static final int splash_background=0x7f020005;
+        public static final int splash_background1=0x7f020006;
     }
     public static final class id {
         public static final int aboutUs=0x7f0b0023;
