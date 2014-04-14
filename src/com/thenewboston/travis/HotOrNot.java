82c85495b40678ed7e39638a04f0e2c43b296a72
Gharbi Mohammed <simo163@hotmail.co.uk>
2014-04-14 10:56:10 +0200

    Set up sqlite databases variables

--- src/com/thenewboston/travis/HotOrNot.java
+++ src/com/thenewboston/travis/HotOrNot.java
@@ -2,6 +2,12 @@ package com.thenewboston.travis;
 
 public class HotOrNot {
 
+	public static final String KEY_ROWID = "_id";
+	public static final String KEY_NAME = "persons_name";
+	public static final String KEY_HOTNESS = "persons_hotness";
 	
+	private static final String DATABASE_NAME = "HotOrNotdb";
+	private static final String DATABASE_Table = "peopleTable";
+	private static final int DATABASE_VERSION = 1;
 	
 }
