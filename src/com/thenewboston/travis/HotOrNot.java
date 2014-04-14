6c2fbc8de27546865968920657dac7c521dfa612
Gharbi Mohammed <simo163@hotmail.co.uk>
2014-04-14 11:03:44 +0200

    SQLiteOpenHelper

--- src/com/thenewboston/travis/HotOrNot.java
+++ src/com/thenewboston/travis/HotOrNot.java
@@ -1,5 +1,10 @@
 package com.thenewboston.travis;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
 public class HotOrNot {
 
 	public static final String KEY_ROWID = "_id";
@@ -9,5 +14,34 @@ public class HotOrNot {
 	private static final String DATABASE_NAME = "HotOrNotdb";
 	private static final String DATABASE_Table = "peopleTable";
 	private static final int DATABASE_VERSION = 1;
+	
+	private DbHelper ourHelper;
+	private final Context ourContext;
+	private SQLiteDatabase ourDatabase;
+	
+	private static class DbHelper extends SQLiteOpenHelper{
+
+		public DbHelper(Context context) {
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	public HotOrNot(Context c){
+		ourContext = c;
+	}
 	
 }
