b39b3fc89c7ac81a998b0ef2fa41ecf584f40499
Gharbi Mohammed <simo163@hotmail.co.uk>
2014-04-16 01:33:54 +0200

    Wacklock to keep you up from sleeping

--- src/com/thenewboston/travis/GFX.java
+++ src/com/thenewboston/travis/GFX.java
@@ -1,20 +1,37 @@
 package com.thenewboston.travis;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 
 public class GFX extends Activity {
 	
-		
 	MyBringBack ourView;
+	WakeLock wL;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
+		
+		//wake-lock
+		PowerManager pM = (PowerManager)getSystemService(Context.POWER_SERVICE);
+		wL = pM.newWakeLock(PowerManager.FULL_WAKE_LOCK, "whatever");
+		
 		super.onCreate(savedInstanceState);
+		wL.acquire();
 		ourView = new MyBringBack(this);
 		setContentView(ourView);
 	}
 
+	@Override
+	protected void onPause() {
+		super.onPause();
+		wL.release();
+	}
+
+	
 	
 }
