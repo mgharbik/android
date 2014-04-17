a6303edc5f3f6574bedcecb56f2e4a3984d8ffd3
Gharbi Mohammed <simo163@hotmail.co.uk>
2014-04-17 03:29:08 +0200

    WebView navigating methods

--- src/com/thenewboston/travis/SimpleBrowser.java
+++ src/com/thenewboston/travis/SimpleBrowser.java
@@ -2,11 +2,17 @@ package com.thenewboston.travis;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class SimpleBrowser extends Activity {
+public class SimpleBrowser extends Activity implements OnClickListener {
 	
 	WebView ourBrowser;
+	EditText url;
+	Button go, back, refresh, foward, clearHistory;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,45 @@ public class SimpleBrowser extends Activity {
 		
 		ourBrowser = (WebView) findViewById(R.id.wvBrowser);
 		ourBrowser.loadUrl("http://google.com");
+		
+		go = (Button) findViewById(R.id.btGo);
+		back = (Button) findViewById(R.id.bBack);
+		refresh = (Button) findViewById(R.id.bRefresh);
+		foward = (Button) findViewById(R.id.bForward);
+		clearHistory = (Button) findViewById(R.id.bHistory);
+		url = (EditText) findViewById(R.id.etURL);
+		
+		go.setOnClickListener(this);
+		back.setOnClickListener(this);
+		refresh.setOnClickListener(this);
+		foward.setOnClickListener(this);
+		clearHistory.setOnClickListener(this);
+		
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		switch (arg0.getId()) {
+		case R.id.btGo:
+			String theWebSite =  url.getText().toString();
+			ourBrowser.loadUrl(theWebSite);
+			break;
+		case R.id.bBack:
+			if (ourBrowser.canGoBack())
+				ourBrowser.goBack();
+			break;
+		case R.id.bRefresh:
+			if (ourBrowser.canGoForward())
+				ourBrowser.goForward();
+			break;
+		case R.id.bForward:
+			ourBrowser.reload();
+			break;
+		case R.id.bHistory:
+			ourBrowser.clearHistory();
+			break;
+		}
 	}
 	
 	
