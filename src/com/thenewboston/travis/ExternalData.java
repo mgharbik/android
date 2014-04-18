d22bfb07e0c7defd765c78f2b981538784b98b17
Gharbi Mohammed <simo163@hotmail.co.uk>
2014-04-18 16:12:11 +0200

    onItemSelected and File Directories

--- src/com/thenewboston/travis/ExternalData.java
+++ src/com/thenewboston/travis/ExternalData.java
@@ -1,19 +1,25 @@
 package com.thenewboston.travis;
 
+import java.io.File;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class ExternalData extends Activity {
+public class ExternalData extends Activity implements OnItemSelectedListener {
 
 	private TextView canWrite, canRead;
 	private String state;
 	private boolean canW, canR;
 	Spinner spinner;
 	String[] paths = { "Music", "Pictures", "Download" };
+	File path = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +28,7 @@ public class ExternalData extends Activity {
 		setContentView(R.layout.externaldata);
 		canWrite = (TextView) findViewById(R.id.tvCanWrite);
 		canRead = (TextView) findViewById(R.id.tvCanRead);
-		
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(ExternalData.this, android.R.layout.simple_spinner_item, paths);
-		
-		spinner = (Spinner) findViewById(R.id.spinner1);
-		spinner.setAdapter(adapter);
+
 		
 		state = Environment.getExternalStorageState();
 		if (state.equals(Environment.MEDIA_MOUNTED)){
@@ -45,6 +47,36 @@ public class ExternalData extends Activity {
 			canRead.setText("false");
 			canW = canR = false;
 		}
+		
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(ExternalData.this, android.R.layout.simple_spinner_item, paths);
+		spinner = (Spinner) findViewById(R.id.spinner1);
+		spinner.setAdapter(adapter);
+		spinner.setOnItemSelectedListener(this);
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+			long arg3) {
+		// TODO Auto-generated method stub
+		int position = spinner.getSelectedItemPosition();
+		switch (position) {
+		case 0:
+			path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC);
+			break;
+		case 1:
+			path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+			break;
+		case 2:
+			path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+			break;
+		}
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	
