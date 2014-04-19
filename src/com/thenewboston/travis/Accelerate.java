543df97ad1b75768016f32c1713b8564a118d8a1
Gharbi Mohammed <simo163@hotmail.co.uk>
2014-04-19 13:40:40 +0200

    125. Setting up a Accelerometer class

--- src/com/thenewboston/travis/Accelerate.java
+++ src/com/thenewboston/travis/Accelerate.java
@@ -1,21 +1,72 @@
 package com.thenewboston.travis;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 public class Accelerate extends Activity implements SensorEventListener{
 
 	// WE SHOULD COPY HERE THE MyBringBackSurface CLASS FROM GFXSURFACE
 	// PROBABLY IT WAS IMPLEMENTED IN BETWEEN 70-110 TUTORIALS
+	public class MyBringBackSurface extends SurfaceView implements Runnable {
+
+		SurfaceHolder ourHolder;
+		Thread ourThread = null;
+		boolean isRunning = false;
+		
+		public MyBringBackSurface(Context context) {
+			// TODO Auto-generated constructor stub
+			super(context);
+			ourHolder = getHolder();
+		}
+
+		public void pause(){
+			isRunning = false;
+			while(true){
+				try {
+					ourThread.join();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			}
+			ourThread = null;
+		}
+		
+		public void resume(){
+			isRunning = true;
+			ourThread = new Thread(this);
+			ourThread.start();
+		}
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			while(isRunning){
+				if (!ourHolder.getSurface().isValid())
+					continue;
+				
+				Canvas canvas = ourHolder.lockCanvas();
+				canvas.drawRGB(02, 02, 150);
+				
+				ourHolder.unlockCanvasAndPost(canvas);
+			}
+		}
+
+	}
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		//setContentView(new MyBringBackSurface(this));
+		setContentView(new MyBringBackSurface(this));
 	}
 
 	@Override
