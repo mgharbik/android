09f853eec93df1afa01e250ab3f013bd9bde3151
Gharbi Mohammed <simo163@hotmail.co.uk>
2014-04-20 03:06:22 +0200

    Start Element method

--- src/com/thenewboston/travis/HandlingXMLStuff.java
+++ src/com/thenewboston/travis/HandlingXMLStuff.java
@@ -1,6 +1,28 @@
 package com.thenewboston.travis;
 
-public class HandlingXMLStuff {
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class HandlingXMLStuff extends DefaultHandler {
+
+	XMLDataCollected info = new XMLDataCollected();
+	
+	@Override
+	public void startElement(String uri, String localName, String qName,
+			Attributes attributes) throws SAXException {
+		// TODO Auto-generated method stub
+		
+		if ( localName.equals("city")){
+			String city = attributes.getValue("data");
+			info.setCity(city);
+		}else if (localName.equals("temp_f")){
+			String temp = attributes.getValue("data");
+			int temp_i = Integer.parseInt(temp);
+			info.setTemp(temp_i);
+		}
+		
+	}
 	
 	
 	
